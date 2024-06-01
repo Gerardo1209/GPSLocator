b38b1bead107352081a5e744ca795f88817367cb
Gerardo Femat Delgado <git@femat.dev>
2024-06-01 12:46:24 -0600

    send data to db

---
+++ GPS Server/src/index.js
@@ -0,0 +1,135 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const fs = require('fs').promises;
+const mysql = require('mysql2/promise');
+const { error } = require('console');
+
+//Puerto
+const PORT = process.env.PORT || 3000;
+
+//Base de datos
+const config = {
+    host: 'uaaantojos.femat.dev',
+    user: 'gerardo',
+    database: 'locaciones_electronica',
+    password: 'FDelgadoG1209M#',
+    port: '3306'
+}
+
+async function conexionBD(){
+    try{
+        const connection = await mysql.createConnection(config);
+        console.log("Se establecio la conexión con la base de datos");
+        return connection;
+    }catch(err){
+        console.log("Error al conectar con la base de datos: ", err);
+    }
+}
+
+
+//Configuración
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+
+/*async function checkFileExist(file){
+    try{
+        await fs.readFile(file);
+        return true;
+    }catch(err){
+        if(err.code == "ENOENT"){
+            await fs.writeFile(file, JSON.stringify([]));
+            return true;
+        }else{
+            return false;
+        }
+    }
+}*/
+
+app.get('/location/:locationString', async (req, res) => {
+    //console.log(req.params.locationString);
+    var data = req.params.locationString.split(',');
+    var lat = data[2];
+    var posLat = data[3];
+    var lon = data[4];
+    var posLon = data[5];
+    var resultLat;
+    var resultLon;
+    if(lat){
+        var dda = lat / 100;
+        dda = parseInt(dda.toString());
+        var ssa = lat - dda * 100;
+        var decLat = dda + ssa / 60;
+        if(posLat == "S"){
+            decLat = decLat * -1;
+        }
+        resultLat = decLat.toFixed(8);
+        //var ss = c
+    }
+    if(lon){
+        var ddo = lon / 100;
+        ddo = parseInt(ddo.toString());
+        var sso = lon - ddo * 100;
+        var decLon = ddo + sso / 60;
+        if(posLon == "W"){
+            decLon = decLon * -1;
+        }
+        resultLon = decLon.toFixed(8);
+    }
+    var ruta = __dirname + "/../data/gpsData.json";
+    //Almacenamiento en archivos
+    /*if(await checkFileExist(ruta)){
+        var locations = await fs.readFile(ruta);
+        var posiciones = JSON.parse(locations);
+        posiciones.push({
+            fecha: new Date().toISOString(),
+            latitud: resultLat,
+            longitud: resultLon
+        });
+        await fs.writeFile(ruta, JSON.stringify(posiciones));
+    }else{
+        res.send({success: false, data: "Error al abrir el archivo contenedor del GPS"});
+    }*/
+
+    //Almacenamiento en base de datos
+    if(resultLat && resultLon){
+        try{
+            await conexionBD().then(async conexion =>{
+                const result = await conexion.query(
+                    'INSERT INTO Ubicacion (Fecha, Altitud, Longitud) VALUES (?,?,?);',
+                    [new Date(), resultLat, resultLon]
+                );
+                console.log(new Date().toISOString() + ": Se escribieron datos correctamente");
+                res.send({success: true, data: "Datos recibidos: "+req.params.locationString});
+            }).catch(error => {
+                res.send({success: false, data: error.message});
+            })
+            
+        }catch(err){
+            res.send({success: false, data: err.message});
+        }
+    }else{
+        res.send({success:false, data: "No se enviaron datos vacios"});
+    }
+});
+
+app.get('/consult', async (req,res) => {
+    try{
+        await conexionBD().then(async conexion =>{
+            const result = await conexion.query(
+                'SELECT * FROM Ubicacion;'
+            );
+            res.send({success: true, data: result[0]});
+        }).catch(error => {
+            res.send({success: false, data: error.message});
+        })
+        
+    }catch(err){
+        res.send({success: false, data: err.message});
+    }
+});
+
+app.listen(PORT, ()=>{
+    console.log("Servidor escuchando en: " + PORT);
+})
---
+++ GPS2/src/main.cpp
@@ -0,0 +1,156 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+// Definir los pines para UART
+#define MODEM_RX_PIN 20
+#define MODEM_TX_PIN 21
+#define MAX_TOKENS 20
+
+//Funciones
+void httpGETRequest(String serverName);
+
+//Wifi data
+const char* ssid = "INFINITUM129D_2.4";
+const char* password = "mxMrVUe3J2";
+WiFiClient client;
+
+//Send data interval
+int actualMillis = 0;
+const int millisDiff = 10000;
+
+//Your Domain name with URL path or IP address with path
+const char* serverName = "http://192.168.1.165:3000/location/";
+
+// Crear una instancia de HardwareSerial
+HardwareSerial modem(1); // Utiliza UART1
+
+void setup() {
+  // Iniciar la comunicación serial con el PC
+  Serial.begin(115200);
+  while (!Serial) { ; } // Esperar a que se inicie el puerto serial
+
+
+  //Inicar WiFi
+  delay(5000);
+  Serial.print("Conectando a WiFi");
+  WiFi.begin(ssid, password);
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println(WiFi.status());
+    //Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+  /*WiFi.mode(WIFI_STA);
+  WiFi.disconnect();*/
+  delay(100);
+
+
+  // Iniciar la comunicación serial con el módem
+  modem.begin(115200, SERIAL_8N1, MODEM_RX_PIN, MODEM_TX_PIN);
+  Serial.print("Comunicando con modem ");
+  while(!modem){
+    Serial.print(".");
+  }
+
+  //Iniciar GPS
+  Serial.println("Iniciando comunicación con el módem...");
+  bool atReady = false;
+  Serial.print("Conectando GPS");
+  modem.println("AT+CGNSSPWR=1");
+  while(!atReady){
+    delay(500);
+    //Serial.print(".");
+    String modemData;
+    if (modem.available()){
+      modemData = modem.readStringUntil('\n');
+      Serial.println(modemData);
+    }
+    if(modemData.startsWith("$G")){
+        atReady = true;
+    }
+    if(modemData.compareTo("+CGNSSPWR: READY!") == 13){
+      Serial.println("Configurando comunicación");
+      delay(100);
+      modem.println("AT+CGNSSPORTSWITCH=1,1");
+      delay(400);
+      modem.println("AT+CGNSSTST=1");
+      delay(400);
+      atReady = true;
+    }
+  }
+}
+
+void loop() {
+  /*if (WiFi.status() == WL_CONNECTED) { // Verifica si está conectado a WiFi
+    HTTPClient http;
+    WiFiClient client;
+    http.begin(client, serverName); // URL del servidor
+    int httpCode = http.GET(); // Realiza la petición
+
+    if (httpCode > 0) { // Si la petición fue exitosa
+      String payload = http.getString(); // Obtiene el cuerpo de la respuesta
+      Serial.println(payload); // Imprime el contenido de la respuesta
+    } else {
+      Serial.println("Error en la petición HTTP");
+    }
+
+    http.end(); // Libera los recursos
+  }
+  delay(1000);*/
+    // Leer datos del módem y enviarlos al puerto serial del PC
+    if (modem.available()) {
+        String modemInfo = modem.readStringUntil('\n');
+        Serial.println(modemInfo);
+        if(modemInfo.startsWith("$GNRMC")){
+            
+            
+            //httpGETRequest(serverName);
+            String tokens[MAX_TOKENS];
+            int tokenCount = 0;
+            int startIndex = 0;
+            int endIndex = modemInfo.indexOf(',');
+
+            while (endIndex != -1) {
+                // Extraer el token
+                tokens[tokenCount] = modemInfo.substring(startIndex, endIndex);
+                tokenCount++;
+                startIndex = endIndex + 1;
+                endIndex = modemInfo.indexOf(',', startIndex);
+            }
+            // Agregar el último token después de la última coma
+            if (startIndex < modemInfo.length()) {
+                tokens[tokenCount] = modemInfo.substring(startIndex);
+                tokenCount++;
+            }
+            //Envia al servidor los datos según los millis determinados
+            if(millis() - actualMillis > millisDiff){
+              actualMillis = millis();
+              String url = serverName;
+              url.concat(tokens[1] + "," + tokens[2] + "," + tokens[3] + "," + tokens[4] + "," + tokens[5] + "," + tokens[6]);
+              if (WiFi.status() == WL_CONNECTED) { // Verifica si está conectado a WiFi
+                HTTPClient http;
+                http.begin(client, url); // URL del servidor
+                int httpCode = http.GET(); // Realiza la petición
+                if (httpCode > 0) { // Si la petición fue exitosa
+                  String payload = http.getString(); // Obtiene el cuerpo de la respuesta
+                  Serial.println(payload); // Imprime el contenido de la respuesta
+                } else {
+                  Serial.println("Error en la petición HTTP");
+                }
+                http.end(); // Libera los recursos
+              }
+            }
+            
+        }
+    }
+
+    // Leer datos del puerto serial del PC y enviarlos al módem
+    if (Serial.available()) {
+        String pcData = Serial.readStringUntil('\n');
+        Serial.print("Envaindo mensaje: ");
+        Serial.println(pcData);
+        modem.println(pcData);
+    }
+}
